eda0ac792e1e4f985a85b53f559e15207cc51c33
techchrism <chris@techchrism.me>
2024-11-17 12:44:46 -0800

    add playlists table and download functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,55 @@
-import type { Component } from 'solid-js'
+import { createSignal, Show, type Component } from 'solid-js'
 import { IoOpenOutline } from 'solid-icons/io'
-import FileUpload from "./components/FileUpload"
+import FileUpload from './components/FileUpload'
+import PlaylistTable from './components/PlaylistsTable'
+import { ParsedZip } from './parse/parseZip';
 
 const App: Component = () => {
-  return (
-    <div class="hero mt-20 bg-base-200">
-      <div class="hero-content text-center">
-        <div class="max-w-2xl">
-          <h1 class="text-5xl font-bold">YouTube Activity Export</h1>
-          <p class="py-6 text-lg">
-            This is a simple tool to view and export videos from the YouTube activity log.
-            It enables exporting a list of liked videos, even if the length is over 5,000 (YouTube API limit).
-          </p>
-          <h2 class="text-3xl font-semibold mb-2">Instructions</h2>
-          <div class="w-fit mx-auto text-lg text-left">
-            <ol class="list-decimal pl-8">
-              <li>Go to <a href="https://takeout.google.com/" class="underline" target="_blank" rel="noopener noreferrer"><span class="underline">Google Takeout </span><IoOpenOutline class="inline"/></a></li>
-              <li>Under "Create a new export", click "Deselect all"</li>
-              <li>
-                Scroll down to "My Activity", select its checkbox, and underneath...
-                <ol class="list-decimal pl-8">
-                  <li>click "Multiple formats" then for "Activity Records", select "JSON" as the format and click "OK"</li>
-                  <li>click "All activity data included" then click "Deselect all" and select just "YouTube" then click "OK"</li>
-                </ol>
-              </li>
-              <li>
-                Scroll down to "YouTube and YouTube Music", select its checkbox, and underneath click "All YouTube data included" then click "Deselect all" and select just "playlists" then click "OK"
-              </li>
-              <li>Click "Next Step"</li>
-              <li>Leave the settings on their defaults (export once, file type zip, file size 2GB)</li>
-              <li>Click "Create Export" and wait for the email indicating your data download is ready! For my data, it took about 10 minutes.</li>
-            </ol>
-          </div>
-          <div class="divider"/>
-          <FileUpload/>
+    const [parsedZip, setParsedZip] = createSignal<ParsedZip | undefined>(undefined)
+
+    return (
+        <div class="hero my-20 bg-base-200 text-lg">
+            <div class="hero-content text-center">
+                <div class="max-w-2xl">
+                    <h1 class="text-5xl font-bold">YouTube Activity Export</h1>
+                    <p class="py-6">
+                        This is a simple tool to view and export videos from the YouTube activity log.
+                        It enables exporting a list of liked videos, even if the length is over 5,000 (YouTube API limit).
+                    </p>
+                    <h2 class="text-3xl font-semibold mb-2">Instructions</h2>
+                    <div class="w-fit mx-auto text-left">
+                        <ol class="list-decimal pl-8">
+                            <li>Go to <a href="https://takeout.google.com/" class="underline" target="_blank" rel="noopener noreferrer"><span class="underline">Google Takeout </span><IoOpenOutline class="inline" /></a></li>
+                            <li>Under "Create a new export", click "Deselect all"</li>
+                            <li>
+                                Scroll down to "My Activity", select its checkbox, and underneath...
+                                <ol class="list-decimal pl-8">
+                                    <li>click "Multiple formats" then for "Activity Records", select "JSON" as the format and click "OK"</li>
+                                    <li>click "All activity data included" then click "Deselect all" and select just "YouTube" then click "OK"</li>
+                                </ol>
+                            </li>
+                            <li>
+                                Scroll down to "YouTube and YouTube Music", select its checkbox, and underneath click "All YouTube data included" then click "Deselect all" and select just "playlists" then click "OK"
+                            </li>
+                            <li>Click "Next Step"</li>
+                            <li>Leave the settings on their defaults (export once, file type zip, file size 2GB)</li>
+                            <li>Click "Create Export" and wait for the email indicating your data download is ready! For my data, it took about 10 minutes.</li>
+                        </ol>
+                    </div>
+
+                    <div class="divider" />
+                    <FileUpload onParse={setParsedZip}/>
+
+                    <Show when={parsedZip()}>
+                        {(parsed) => <>
+                            <div class="divider" />
+                            <PlaylistTable parsedZip={parsed()} />
+                        </>}
+                    </Show>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default App;
---
+++ src/components/PlaylistsTable.tsx
@@ -0,0 +1,93 @@
+import {createEffect, createMemo, createSignal, For, type Component} from 'solid-js'
+import {ParsedZip} from '../parse/parseZip'
+import uzip from 'uzip'
+import filenamify from 'filenamify'
+
+export type PlaylistTableProps = {
+    parsedZip: ParsedZip
+}
+
+const PlaylistTable: Component<PlaylistTableProps> = (props) => {
+    const [selected, setSelected] = createSignal<boolean[]>([])
+    createEffect(() => {
+        setSelected(props.parsedZip.playlists.map(p => true))
+    })
+
+    const selectedCount = createMemo(() => selected().reduce((result, checked) => result + (checked ? 1 : 0), 0))
+    const allSelected = createMemo(() => selectedCount() === props.parsedZip.playlists.length)
+
+    function toggleSelected(index: number) {
+        setSelected(prev => {
+            const newArr = [...prev]
+            newArr[index] = !newArr[index]
+            return newArr
+        })
+    }
+
+    function setAllSelected(value: boolean) {
+        setSelected(selected().map(_ => value))
+    }
+
+    function download() {
+        const files: Record<string, Uint8Array> = {}
+
+        props.parsedZip.playlists.filter((_, i) => selected()[i]).forEach(playlist => {
+            const safeName = filenamify(playlist.name.replace(/[\W_]+/g, '_'), {replacement: '_'})
+            files[`playlists/${safeName}.txt`] = new TextEncoder().encode(playlist.videos.map(v => v.url).join('\n'))
+            files[`playlists/${safeName}.json`] = new TextEncoder().encode(JSON.stringify(playlist))
+        })
+
+        files['combined.txt'] = new TextEncoder().encode(Array.from(props.parsedZip.playlists
+            .filter((_, i) => selected()[i])
+            .map(p => p.videos.map(v => v.url))
+            .reduce((result, urls) => {
+                urls.forEach(url => result.add(url))
+                return result
+            }, new Set<string>()))
+            .join('\n'))
+
+        const zip = uzip.encode(files)
+
+        const downloadUrl = URL.createObjectURL(new Blob([new DataView(zip)], {type: 'application/zip'}))
+        const a = document.createElement('a')
+        a.href = downloadUrl
+        a.download = `playlist${selectedCount() === 1 ? '' : 's'}.zip`
+        document.body.appendChild(a)
+        a.click()
+        URL.revokeObjectURL(downloadUrl)
+        document.body.removeChild(a)
+    }
+
+    return (
+        <>
+            <table class="table text-xl mb-4">
+                <thead class="text-lg">
+                    <tr>
+                        <th>
+                            <input type="checkbox" title="Toggle All" class="checkbox" checked={allSelected()} onClick={() => setAllSelected(!allSelected())}/>
+                        </th>
+                        <th>Playlist Name</th>
+                        <th>Videos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <For each={props.parsedZip.playlists}>
+                        {(item, index) => <tr>
+                            <td>
+                                <input type="checkbox" class="checkbox" checked={selected()[index()]} onClick={() => toggleSelected(index())}/>
+                            </td>
+                            <td>{item.name}</td>
+                            <td>{item.videos.length}</td>
+                        </tr>}
+                    </For>
+                </tbody>
+            </table>
+
+            <button class="btn btn-success btn-lg" disabled={selectedCount() === 0} onClick={download}>
+                Download {selectedCount()} playlist{selectedCount() === 1 ? '' : 's'}
+            </button>
+        </>
+    )
+}
+
+export default PlaylistTable
