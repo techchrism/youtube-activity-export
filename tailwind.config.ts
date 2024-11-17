6c9d1d60e6922d6a89a286eea3ce6ee85dbdb5a7
techchrism <chris@techchrism.me>
2024-11-16 16:59:45 -0800

    initial template with daisyui, solid-icons, uzip, and papaparse

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
+
+const config: Config = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui
+  ],
+};
+
+export default config;
